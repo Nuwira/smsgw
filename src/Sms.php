2c0c2368000172b86e5b0e5e4fae482d2fa40705
Muhammad Zamroni <halo@matriphe.com>
2016-01-02 17:40:22 +0700

    Add pretend capabilities

--- src/Sms.php
+++ src/Sms.php
@@ -13,11 +13,10 @@ use GuzzleHttp\Exception\ClientException;
 
 class Sms
 {
-    protected $base_url = 'http://apisms.nuwira.net/';
-    
+    protected $base_url;
     protected $client_id;
-    
     protected $client_secret;
+    protected $pretend;
     
     protected $scope = [
         'grant_type' => 'client_credentials',
@@ -25,17 +24,20 @@ class Sms
     ];
     
     protected $guzzle;
-    
     protected $format;
     
     protected $token;
-    
     protected $cache_key = 'nuwira_sms_access_token';
+    protected $pretend_cache_key = 'nuwira_sms_pretend';
     
     public function __construct()
     {
+        $this->base_url = Config::get('sms.base_url');
+        
         $this->client_id = Config::get('sms.client_id');
         $this->client_secret = Config::get('sms.client_secret');
+        
+        $this->pretend = Config::get('sms.pretend');
         
         $this->guzzle = new Client([
             'base_uri' => $this->base_url,
@@ -47,6 +49,10 @@ class Sms
     
     public function send($phone_number, $message)
     {
+        if ($this->pretend) {
+            return $this->pretendSend($phone_number, $message);
+        }
+        
         $phone_number = $this->format->phone($phone_number);
         
         $message = trim($message);
@@ -80,6 +86,10 @@ class Sms
     
     public function check($message_id)
     {
+        if ($this->pretend) {
+            return $this->pretendCheck($message_id);
+        }
+        
         $token = $this->getToken();
         
         $query = [
@@ -112,6 +122,64 @@ class Sms
         }
         
         return $this->token;
+    }
+    
+    public function setPretend($pretend)
+    {
+        $this->pretend = (bool) $pretend;
+    }
+    
+    protected function pretendSend($phone_number, $message)
+    {
+        $phone_number = $this->format->phone($phone_number);
+        
+        $message = trim($message);
+        $message = substr($message, 0, 160);
+        
+        $inboxes = collect(Cache::get($this->pretend_cache_key, []));
+        $message_id = $inboxes->count();
+        $message_id++;
+        
+        $inboxes->push([
+            'message_id' => $message_id,
+            'message' => $message,
+            'phone_number' => $phone_number,
+            'sending_datetime' => date('Y-m-d H:i:s'),
+            'status' => 'Sent',
+        ]);
+        Cache::put($this->pretend_cache_key, $inboxes->toArray(), (60*60));
+        
+        $output = [
+            'status' => 200,
+            'message' => 'Your message has been queuing on outbox',
+            'message_id' => [$message_id],
+        ];
+        
+        return collect($output)->toArray();
+    }
+    
+    protected function pretendCheck($message_id)
+    {
+        $inboxes = collect(Cache::get($this->pretend_cache_key, []));
+        
+        $message = $inboxes->filter(function($m) use ($message_id) {
+            return $i['message_id'] = $message_id;
+        })->first();
+        
+        if (empty($message)) {
+            $output = [
+                'status' => 400,
+                'message' => 'Message not found',
+            ];
+        } else {
+            $output = [
+                'status' => 200,
+                'message' => 'Detail of your message',
+                'data' => $message->except('message_id')->toArray(),
+            ];
+        }
+        
+        return collect($output)->toArray();
     }
     
     protected function login()
--- src/config/config.php
+++ src/config/config.php
@@ -1,6 +1,10 @@
 <?php
 
 return [
+    'base_url' => env('NUWIRA_SMSGW_BASE_URL', 'http://apisms.nuwira.net/'),
+    
     'client_id' => env('NUWIRA_SMSGW_CLIENT_ID', 'client_id'),
-    'client_secret' => env('NUWIRA_SMSGW_CLIENT_SECRET', 'client_secret')    
+    'client_secret' => env('NUWIRA_SMSGW_CLIENT_SECRET', 'client_secret'),
+    
+    'pretend' => env('NUWIRA_SMSGW_PRETEND', false),
 ];
