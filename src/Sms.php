99d6560f5fdc247c0730d95e28b1ecf858b0d3f5
Amalina Kurniasari <amalinakurniasari@gmail.com>
2018-01-16 13:50:16 +0700

    init

---
+++ src/Sms.php
@@ -0,0 +1,106 @@
+<?php
+namespace Nuwira\Smsgw;
+
+use GuzzleHttp\Client;
+use libphonenumber\PhoneNumberUtil;
+use libphonenumber\PhoneNumberFormat;
+use Illuminate\Support\Facades\Config;
+use Cache;
+use Exception;
+use GuzzleHttp\Exception\ClientException;
+
+class Sms
+{
+    protected $base_url;
+    protected $api_key;
+    protected $to;
+    protected $message;
+    
+    public function __construct()
+    {
+        $this->base_url = Config::get('sms.base_url');
+        $this->api_key = Config::get('sms.api_key');
+        
+        $this->guzzle = new Client([
+            'base_uri' => $this->base_url,
+            'timeout' => 60,
+            'http_errors' => false,
+            'headers' => [
+                'Authorization'     => 'Token '.$this->api_key
+            ]
+        ]);
+    }
+
+    public function to($phone_number)
+    {
+        $this->to = $phone_number;
+        return $this;
+    }
+
+    public function text($message)
+    {
+        $this->text = $message;
+        return $this;
+    }
+    
+    public function send($phone_number=null, $message=null)
+    {
+        $phone_number   = !empty($phone_number) ? $phone_number : $this->to;
+        $message = !empty($message) ? $message : $this->text;
+
+        $phone_number = $this->formatPhone($phone_number);
+        
+        try {
+            $response = $this->guzzle->post('/send', [
+                'form_params' => [
+                    'to' => $phone_number,
+                    'msg' => $message,
+                ]
+            ]);
+            $data = $response->getBody();
+            
+            return json_decode($data);
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
+    
+    public function check($message_id)
+    {        
+        try {
+            $response = $this->guzzle->get('check/', [
+                'query' => [
+                    'id' => $message_id
+                ]
+            ]);
+            $data = $response->getBody();
+            
+            return json_decode($data);
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
+    
+    protected function formatPhone($phone_number)
+    {
+        $locale = strtoupper(app()->getLocale());
+    
+        $phoneUtil = PhoneNumberUtil::getInstance();
+        
+        try {
+            $phone = $phoneUtil->parse($phone_number, $locale);
+            
+            $is_valid = $phoneUtil->isValidNumber($phone, $locale);
+            
+            if ($is_valid) {
+                return phone_format(
+                    $phone_number, $locale, PhoneNumberFormat::INTERNATIONAL
+                );
+            } else {
+                return $phone_number;
+            }    
+        } catch (Exception $e) {
+            return $phone_number;
+        }
+    }
+}
---
+++ src/config/sms.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'base_url' => env('NUWIRA_SMS_URL', 'http://dev.smsbridge.nuwira.net/'),
+    'api_key' => env('NUWIRA_SMS_API_KEY'),
+];
